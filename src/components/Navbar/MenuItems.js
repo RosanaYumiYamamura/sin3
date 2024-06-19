e1f63c8e167d9bf879d2a81adf2378db2406a99c
Pepe <studio7dos7@gmail.com>
2024-06-19 18:51:08 -0300

    first-commint

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url:'https://sites.google.com/view/kitchenkitchen123456/inicio',
+        cName:'nav-links'
+    },
+    {
+        title: 'Service',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Products',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title: 'Sign Up',
+        url:'#',
+        cName:'nav-links-mobile'
+    }
+]
